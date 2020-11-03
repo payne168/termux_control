42d82537d2beb4fabf76d36db1d78875bea0db09
payne <leipayne18@gmail.com>
2020-11-03 20:37:29 +0800

    ai code

--- bots/verification/verification_code_ccb.py
+++ bots/verification/verification_code_ccb.py
@@ -79,8 +79,8 @@ class VerificationCodeCcb:
     def image_str(self):
         image = self.delete_spot()
         # pytesseract.pytesseract.tesseract_cmd = r"/usr/local/bin/tesseract"  # 设置pyteseract路径
-        result = pytesseract.image_to_string(image, lang='eng', config="--psm 6 --tessdata-dir "
-                                                                       "bots/verification/tessdata")  # 图片转文字
+        result = pytesseract.image_to_string(image, lang='eng', config="--psm 6 --tessdata-dir /data/data/com.termux/files/home/storage/shared/water_drop_init/termux_control/bots/verification/tessdata")  # 图片转文字
+        # result = pytesseract.image_to_string(image, lang='eng', config="--psm 6 --tessdata-dir /tessdata")
         print(result)
         results = re.sub(u"([^\u4e00-\u9fa5\u0030-\u0039\u0041-\u005a\u0061-\u007a])", "", result)  # 去除识别出来的特殊字符
         result_four = results[0:4]  # 只获取前4个字符
