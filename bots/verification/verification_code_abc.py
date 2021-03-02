6cea39598661c1b402c7961a2aac66085d9be021
payne dong <leipayne18@gmail.com>
2021-03-02 15:02:31 +0800

    image read

--- bots/verification/verification_code_abc.py
+++ bots/verification/verification_code_abc.py
@@ -4,6 +4,7 @@ import re  # 用于正则
 from PIL import Image  # 用于打开图片和对图片处理
 import pytesseract  # 用于图片转文字
 import time
+from ..verification.opencv_code import OpencvCode
 
 
 class VerificationCodeAbc:
@@ -80,11 +81,14 @@ class VerificationCodeAbc:
 
     def image_str(self):
         image = self.delete_spot()
+        image.save(self.img)
+        cv_code = OpencvCode(self.img)
+        result_four = cv_code.read_code()
         # pytesseract.pytesseract.tesseract_cmd = r"/usr/local/bin/tesseract"  # 设置pyteseract路径
-        result = pytesseract.image_to_string(image, lang='eng', config="--psm 6 --tessdata-dir bots/verification/tessdata")  # 图片转文字
-        print(result)
-        results = re.sub(u"([^\u4e00-\u9fa5\u0030-\u0039\u0041-\u005a\u0061-\u007a])", "", result)  # 去除识别出来的特殊字符
-        result_four = results[0:10]  # 只获取前4个字符
+        # result = pytesseract.image_to_string(image, lang='eng', config="--psm 6 --tessdata-dir bots/verification/tessdata")  # 图片转文字
+        # print(result)
+        # results = re.sub(u"([^\u4e00-\u9fa5\u0030-\u0039\u0041-\u005a\u0061-\u007a])", "", result)  # 去除识别出来的特殊字符
+        # result_four = results[0:10]  # 只获取前4个字符
         print(result_four)  # 打印识别的验证码
         return result_four
 
